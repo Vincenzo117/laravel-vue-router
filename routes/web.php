ccfcc64817e0d54084ca11a425fcd335c93882b1
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2022-05-04 16:05:42 +0200

    categories migration and seeder

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.home');
 });
 
-Auth::routes();
+Auth::routes(); 
 
 Route::middleware('auth')
     ->namespace('Admin')

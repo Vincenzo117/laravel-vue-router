c079a9a51f7a60f2706a32dd193b6878ed2c0de3
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2022-05-05 16:03:47 +0200

    tag and post many to many relation + post seeder added fake category id and fake tags

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Category;
 use App\Post;
+use App\Tag;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -14,6 +16,12 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $categories = Category::all();
+        $categoriesId = $categories->pluck('id')->all();
+
+        $tags = Tag::all();
+        $tagsId = $tags->pluck('id')->all();
+
         for($i=0; $i < 200; $i++) {
 
             $post = new Post();
@@ -22,8 +30,13 @@ class PostSeeder extends Seeder
             $post->slug = Str::slug($post->title);
             $post->content = $faker->paragraphs(10, true);
             $post->published_at = $faker->randomElement([ null, $faker->dateTime() ]);
+            $post->category_id = $faker->optional()->randomElement( $categoriesId );
+
+            $randomTags = $faker->randomElements( $tagsId, 2);
 
             $post->save();
+
+            $post->tags()->attach( $randomTags );
         }
     }
 }

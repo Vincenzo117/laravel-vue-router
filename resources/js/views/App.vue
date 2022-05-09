357a1adfb9cb0d0706c55e4f541d4bad32f98b5e
Vincenzo117 <94491363+Vincenzo117@users.noreply.github.com>
2022-05-09 16:26:34 +0200

    front.js + App.vue + tailwind cdn

---
+++ resources/js/views/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-xl text-center">App</div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+          theme: {
+            extend: {
+              colors: {
+                clifford: '#da373d',
+              }
+            }
+          }
+        }
+      </script>
     <title>BoolPress</title>
 </head>
 <body>
     <div id="root"></div>
 
-    <script src="{{ asset('js/fron.js') }}"></script>
+    <script src="{{ asset('js/front.js') }}"></script>
 </body>
 </html>
